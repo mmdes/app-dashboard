05b4e280825a9104de4e66acd17c377b7a456da5
Matheus Matos <lematheusm@gmail.com>
2022-04-11 23:06:59 -0400

    usando método post para requisição

--- public/script.js
+++ public/script.js
@@ -3,16 +3,24 @@ $(document).ready(() => {
 
 	$('#documentacao').on('click', ()=>{
         //$('#pagina').load('documentacao.html')
-        $.get('documentacao.html', data => {
+        /*$.get('documentacao.html', data => {
+            $('#pagina').html(data)
+        })*/
+        $.post('documentacao.html', data => {
             $('#pagina').html(data)
         })
     })
 
     $('#suporte').on('click', ()=>{
         //$('#pagina').load('suporte.html')
-        $.get('suporte.html', data => {
+        /*$.get('suporte.html', data => {
+            $('#pagina').html(data)
+        })*/
+
+        $.post('suporte.html', data => {
             $('#pagina').html(data)
         })
+
     })
 
 })
