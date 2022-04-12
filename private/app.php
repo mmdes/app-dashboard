aad73beccf8dbc5d45ecd9d331cd7724f531b5eb
Matheus Matos <lematheusm@gmail.com>
2022-04-11 23:59:43 -0400

    preparando o back-end

---
+++ private/app.php
@@ -0,0 +1,64 @@
+<?php
+class Dashboard{
+
+    public $data_inicio;
+    public $data_fim;
+    public $numeroVendas;
+    public $totalVendas;
+
+    public function __get($atributo){
+        return $this->$atributo;
+    }
+
+    public function __set($atributo,$valor){
+        $this->$atributo = $valor;
+        return $this;
+    }
+}
+
+class Conexao{
+    private $host = 'localhost';
+    private $dbname = 'dashboard';
+    private $user = 'root';
+    private $pass = '';
+
+    public function conectar(){
+        try{
+            $conexao = new PDO(
+                "mysqli:host=$this->host;dbname=$this->dbname",
+                "$this->user",
+                "$this->pass"
+            );
+            //conexão com backend deve usar mesma coleção de caracteres
+
+            $conexao->exec('set charset utf8');
+
+            return $conexao;
+
+        }catch(PDOException $e){
+            echo '<p>'.$e->getMessage().'</p>';
+        }
+    }
+}
+
+//classe (model)
+
+class Bd{
+    private $conexao;
+    private $dashboard;
+
+    public function __construct(Conexao $conexao, Dashboard $dashboard){
+        $this->conexao = $conexao->conectar();
+    }
+
+    $dashboard = new Dashboard();
+
+    $conexao = new Conexao();
+
+    $bd = new Bd($conexao, $dashboard);
+
+
+}
+
+
+?>
